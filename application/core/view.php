b1137c2fa04d687c9e2d77f60a310be09627dee6
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2016-11-19 12:33:36 +0200

    Clean

--- application/core/view.php
+++ application/core/view.php
@@ -1,9 +1,7 @@
 <?php
 
 class View{
-	
 	//public $template_view; // здесь можно указать общий вид по умолчанию.
-	
 	/*
 	$content_file - виды отображающие контент страниц;
 	$template_file - общий для всех страниц шаблон;
@@ -12,18 +10,9 @@ class View{
 	public function generate($content_view, $template_view, $data = null){
 		/*
 		if(is_array($data)) {
-			
-			// преобразуем элементы массива в переменные
 			extract($data);
 		}
 		*/
-		
-		/*
-		динамически подключаем общий шаблон (вид),
-		внутри которого будет встраиваться вид
-		для отображения контента конкретной страницы.
-		*/
-	
 		include 'application/views/'.$template_view;
 	}
 }
