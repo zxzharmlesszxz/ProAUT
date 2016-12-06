776271717b00c3cf3ca1ea2b4c9de8e20e6a454a
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2016-12-06 12:45:53 +0200

    Update controller_users.php
    Remove debug

--- application/controllers/controller_users.php
+++ application/controllers/controller_users.php
@@ -1,40 +1,43 @@
 <?php
 
-class Controller_Users extends Controller{
+/**
+* Controller for User Class
+**/
 
- public function __construct(){
+class Controller_Users extends Controller {
+
+ public function __construct() {
   $this->model = new Model_Users();
   $this->view = new View();
   $this->query = $_REQUEST;
  }
 
- public function action_index(){
+ public function action_index() {
   $data = $this->model->get_data();
   $this->view->generate('users_view.php', 'template_view.php', $data);
  }
 
- public function action_edit(){
+ public function action_edit() {
   $data = $this->model->get($this->query['userid']);
   $this->view->generate('user_edit.php', 'template_view.php', $data);
  }
 
- public function action_changeStatus(){
+ public function action_changeStatus() {
   $data = $this->model->changeStatus($this->query['userid']);
   $this->view->generate('', 'ajax_view.php', $data);
  }
 
- public function action_create(){
-  var_dump($this->query['user']);
+ public function action_create() {
   $data = $this->model->create($this->query['user']);
   $this->view->generate('', 'ajax_view.php', $data);
  }
 
- public function action_delete(){
+ public function action_delete() {
   $data = $this->model->delete($this->query['userid']);
   $this->view->generate('', 'ajax_view.php', $data);
  }
 
- public function action_update(){
+ public function action_update() {
   $data = $this->model->update($this->query['user']);
   $this->view->generate('', 'ajax_view.php', $data);
  }
