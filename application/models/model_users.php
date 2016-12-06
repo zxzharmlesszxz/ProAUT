b7a5c95a542794575e6c8ee6c013803557aeb1cf
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2016-12-06 12:58:43 +0200

    Update model_users.php
    Clean debug

--- application/models/model_users.php
+++ application/models/model_users.php
@@ -24,7 +24,6 @@ class Model_Users extends Model {
 
  public function create(array $user) {
   $user = User::add($user['login'], $user['password'], $user['username'], $user['email'], $user['homedir'], $user['shell'], $user['uid'], $user['gid']);
-  var_dump($user);
   return $user->save() ? $user : false;
  }
 
