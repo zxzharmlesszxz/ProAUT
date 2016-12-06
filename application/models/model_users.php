3eb89149ed4a1a1821a4ff5611fd98845b92e87d
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2016-12-06 10:15:03 +0200

    Update model_users.php
    Clean code

--- application/models/model_users.php
+++ application/models/model_users.php
@@ -1,52 +1,51 @@
 <?php
 
-class Model_Users extends Model{
+class Model_Users extends Model {
  
- public function get_data(){
+ public function get_data() {
   #return User::find_all();
   $users = new Collection;
-  foreach(User::find_all() as $user){
+  foreach (User::find_all() as $user) {
    $users->addItem($user, $user->login);
   }
   return $users;
  }
 
- public function get_user($userid){
+ public function get_user($userid) {
   return User::find_by_id($userid);
  }
 
- public function changeStatus($userid){
+ public function changeStatus($userid) {
   return $user = $this->get_user($userid)->changeStatus();
  }
 
- public function add(array $user){
+ public function add(array $user) {
   $user = User::add($user['login'], $user['password'], $user['username'], $user['email'], $user['homedir'], $user['shell'], $user['uid'], $user['gid']);
   $user->save();
   return $user;
  }
 
- public function delete($userid){
+ public function delete($userid) {
   return User::find_by_id($userid)->delete();
  }
 
- public function update(array $user){
-  foreach($user as $key => $value){
+ public function update(array $user) {
+  foreach ($user as $key => $value) {
    $user[$key] = trim($value);
   }
-  if(($user['password'] == $user['repassword']) && !empty($user['password'])){
+  if (($user['password'] == $user['repassword']) && !empty($user['password'])) {
    unset($user['repassword']);
-  }else{
+  } else {
    unset($user['password']);
    unset($user['repassword']);
   }
   $u = User::find_by_scope(array('login' => $user['login']))[0];
-  if(!$u){
+  if (!$u) {
    return FALSE;
   }
-  var_dump($u);
-  foreach($user as $key => $value){
+  foreach ($user as $key => $value) {
    $u->$key = $value;
-   if($key == 'password'){
+   if ($key == 'password') {
     $u->setPassword($u->password);
    }
   }
