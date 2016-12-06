2755ee9030d39ddfd11ef76cca2b007b0942d3ed
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2016-12-06 11:42:53 +0200

    Update controller_users.php
    Remove old views

--- application/controllers/controller_users.php
+++ application/controllers/controller_users.php
@@ -20,21 +20,21 @@ class Controller_Users extends Controller{
 
  public function action_changeStatus(){
   $data = $this->model->changeStatus($this->query['userid']);
-  $this->view->generate('user_changeStatus.php', 'ajax_view.php', $data);
+  $this->view->generate('', 'ajax_view.php', $data);
  }
 
  public function action_create(){
   $data = $this->model->create($this->query['user']);
-  $this->view->generate('user_add.php', 'ajax_view.php', $data);
+  $this->view->generate('', 'ajax_view.php', $data);
  }
 
  public function action_delete(){
   $data = $this->model->delete($this->query['userid']);
-  $this->view->generate('user_delete.php', 'ajax_view.php', $data);
+  $this->view->generate('', 'ajax_view.php', $data);
  }
 
  public function action_update(){
   $data = $this->model->update($this->query['user']);
-  $this->view->generate('user_update.php', 'ajax_view.php', $data);
+  $this->view->generate('', 'ajax_view.php', $data);
  }
 }
