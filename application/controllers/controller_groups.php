e6639e263b0bab1e3c3fd20089d3dea36ad5e078
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2016-12-06 10:18:04 +0200

    Update controller_groups.php
    Change names of methods

--- application/controllers/controller_groups.php
+++ application/controllers/controller_groups.php
@@ -13,32 +13,32 @@ class Controller_Groups extends Controller{
  }
 
  public function action_edit(){
-  $data = $this->model->get_group($this->query['groupid']);
+  $data = $this->model->get($this->query['groupid']);
   $this->view->generate('group_edit.php', 'template_view.php', $data);
  }
 
  public function action_save(){
-  $data = $this->model->save_group($this->query['groupid']);
+  $data = $this->model->save($this->query['groupid']);
   $this->view->generate('group_save.php', 'template_view.php', $data);
  }
 
  public function action_delete(){
-  $data = $this->model->delete_group($this->query['groupid']);
+  $data = $this->model->delete($this->query['groupid']);
   $this->view->generate('group_delete.php', 'ajax_view.php', $data);
  }
 
  public function action_create(){
-  $data = (!empty($this->query['groupname'])) ? $this->model->create_group($this->query['groupname'], $this->query['gid']) : NULL;
+  $data = (!empty($this->query['groupname'])) ? $this->model->create($this->query['groupname'], $this->query['gid']) : NULL;
   $this->view->generate('group_create.php', 'ajax_view.php', $data);
  }
 
  public function action_addMember(){
-  $data = (!empty($this->query['groupid'])) ? $this->model->addMember_group($this->query['groupid'], $this->query['member']) : NULL;
+  $data = (!empty($this->query['groupid'])) ? $this->model->addMember($this->query['groupid'], $this->query['member']) : NULL;
   $this->view->generate('group_addMember.php', 'ajax_view.php', $data);
  }
 
  public function action_deleteMember(){
-  $data = (!empty($this->query['groupid'])) ? $this->model->deleteMember_group($this->query['groupid'], $this->query['member']) : NULL;
+  $data = (!empty($this->query['groupid'])) ? $this->model->deleteMember($this->query['groupid'], $this->query['member']) : NULL;
   $this->view->generate('group_deleteMember.php', 'ajax_view.php', $data);
  }
 }
