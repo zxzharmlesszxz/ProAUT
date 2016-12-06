17113240051d3341f4175182b4818a94d7904f8d
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2016-12-06 16:43:28 +0200

    Update groups_view.php
    Test EOL

--- application/views/groups_view.php
+++ application/views/groups_view.php
@@ -1,9 +1,9 @@
 <h1>Groups</h1>
 <p>
  <b>Create new group</b>
-  <input tupe="text" name="groupname" value="" />
+  <input tupe="text" name="groupname" value="" placeholder="Groupname" />
   <input type="number" name="gid" value="<?php echo config()->DEFAULT_GID; ?>" />
-  <button id="createGroup">Create</button>
+  <button id="createGroup" title="Create" alt="Create">Create</button>
 </p>
 <p>
 <table id='table' class='display'>
@@ -22,10 +22,15 @@
 
  foreach ($data->keys() as $item) {
   $row = $data->getItem($item);
-  echo '<tr><td>'.$row->groupname.'<span class="actions">
-  <img data-id="'.$row->groupid.'" class="deleteGroup" src="/images/delete.png" title="Delete" alt="Delete"/>
-  <img src="/images/edit.png" title="Edit" alt="Edit" onclick="location.href=\'/groups/edit/?groupid='.$row->groupid.'\'"/></span></td><td>';
-  echo $row->gid.'</td><td>';
+  echo <<EOL
+   <tr>
+    <td>$row->groupname<span class="actions">
+     <img data-id="$row->groupid" class="deleteGroup" src="/images/delete.png" title="Delete" alt="Delete"/>
+     <img src="/images/edit.png" title="Edit" alt="Edit" onclick="location.href='/groups/edit/?groupid=$row->groupid'"/></span>
+    </td>
+    <td>$row->gid</td>
+   <td>
+EOL
   foreach (explode(',', $row->members) as $member) {
    echo empty($member) ? '' : '<a href="/users/show/?login='.$member.'">'.$member.'</a>, ';
   }
