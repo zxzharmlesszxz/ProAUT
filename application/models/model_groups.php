a9534fe15e23d3d200b50eb3dc64a461a67faedb
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2016-12-06 10:13:21 +0200

    Update model_groups.php
    Rename methods

--- application/models/model_groups.php
+++ application/models/model_groups.php
@@ -6,32 +6,32 @@ class Model_Groups extends Model{
   return Group::find_all();
  }
 
- public function get_group($groupid){
+ public function get($groupid){
   return Group::find_by_id($groupid);
  }
 
- public function save_group(Group $group){
+ public function save(Group $group){
   $group->save();
   return $group;
  }
 
- public function delete_group($groupid){
+ public function delete($groupid){
   return Group::find_by_id($groupid)->delete();
  }
 
- public function create_group($groupname, $gid = NULL){
+ public function create($groupname, $gid = NULL){
   $group = Group::add($groupname, $gid);
   $group->save();
   return $group;
  }
 
- public function addMember_group($groupid, $member){
+ public function addMember($groupid, $member){
   $group = Group::find_by_id($groupid);
   $group->addMember(trim($_REQUEST['member']));
   return $group->save();
  }
 
- public function deleteMember_group($groupid, $member){
+ public function removeMember($groupid, $member){
   $group = Group::find_by_id($groupid);
   $group->delMember($member);
   return $group->save();
