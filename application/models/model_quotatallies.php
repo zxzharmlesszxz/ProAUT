bcdf669544ba00b9bb66b573e9fd24665cde8f97
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2016-12-07 10:07:08 +0200

    Update model_quotatallies.php

--- application/models/model_quotatallies.php
+++ application/models/model_quotatallies.php
@@ -1,12 +1,16 @@
 <?php
 
-class Model_Quotatallies extends Model{
+class Model_Quotatallies extends Model {
 
- public function get_data(){
-  return QuotaTally::find_all();
+ public function get_data() {
+  $quotatallys = new Collection;
+  foreach (QuotaTally::find_all() as $quotatally) {
+   $quotatallys->addItem($quotatally, $quotatally->$quotatallyid);
+  }
+  return $quotatallys;
  }
 
- public function delete_group($quotatallyid){
+ public function delete(integer $quotatallyid) {
   return QuotaTally::find_by_id($quotatallyid)->delete();
  }
 }
