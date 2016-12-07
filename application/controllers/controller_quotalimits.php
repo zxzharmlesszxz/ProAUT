0ca0da5a5c9009bda3c2c31e4b94287cde37ec9a
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2016-12-07 10:01:24 +0200

    Update controller_quotalimits.php
    Clean methods names

--- application/controllers/controller_quotalimits.php
+++ application/controllers/controller_quotalimits.php
@@ -13,22 +13,22 @@ class Controller_Quotalimits extends Controller{
  }
 
  public function action_edit(){
-  $data = $this->model->get_quotalimit($this->query['quotalimitid']);
+  $data = $this->model->get($this->query['quotalimitid']);
   $this->view->generate('quotalimit_edit.php', 'template_view.php', $data);
  }
 
  public function action_save(){
-  $data = $this->model->save_quotalimit($this->query['quotalimitid']);
-  $this->view->generate('quotalimit_save.php', 'template_view.php', $data);
+  $data = $this->model->save($this->query['quotalimitid']);
+  $this->view->generate('', 'template_view.php', $data);
  }
 
  public function action_delete(){
-  $data = $this->model->delete_quotalimit($this->query['quotalimitid']);
-  $this->view->generate('quotalimit_delete.php', 'ajax_view.php', $data);
+  $data = $this->model->delete($this->query['quotalimitid']);
+  $this->view->generate('', 'ajax_view.php', $data);
  }
 
  public function action_create(){
-  $data = (!empty($this->query['quotalimit'])) ? $this->model->create_quotalimit($this->query['quotalimit']) : NULL;
-  $this->view->generate('quotalimit_create.php', 'ajax_view.php', $data);
+  $data = (!empty($this->query['quotalimit'])) ? $this->model->create($this->query['quotalimit']) : NULL;
+  $this->view->generate('', 'ajax_view.php', $data);
  }
 }
