831434b42a2004561dd1040b511774db13d12fcb
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2016-12-07 09:58:55 +0200

    Update QuotaLimit.class.php

--- application/classes/QuotaLimit.class.php
+++ application/classes/QuotaLimit.class.php
@@ -9,8 +9,6 @@ class QuotaLimit extends Quota {
  protected static $db_fields = array('quotalimitid', 'name', 'quota_type', 'per_session', 'limit_type', 'bytes_in_avail', 'bytes_out_avail', 'bytes_xfer_avail', 'files_in_avail', 'files_out_avail', 'files_xfer_avail');
 
  public $quotalimitid;
- public $name;
- public $quota_type;
  public $per_session = 'false';
  public $limit_type;
  public $bytes_in_avail = 0;
