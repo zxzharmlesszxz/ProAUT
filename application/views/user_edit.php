7a16f1364218e1b370e7774b052b73ea8560bde8
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2016-12-10 14:49:10 +0200

    Update user_edit.php

--- application/views/user_edit.php
+++ application/views/user_edit.php
@@ -1,3 +1,6 @@
+<?php
+
+echo <<<EOT
 <h1>User</h1>
 <p>
 <table id='1table' class='display'>
@@ -10,20 +13,62 @@
  <th>Value</th>
 </tfoot>
 <tbody>
-<?php
-
-  echo '<tr><td><label for="login">Login</label></td><td><input type="text" readonly="true" value="'.$data->login.'" name="user[login]"/></td></tr>';
-  echo '<tr><td><label for="username">User Name</label></td><td><input type="text" value="'.$data->username.'" name="user[username]"/></td></tr>';
-  echo '<tr><td><label for="email">Email</label></td><td><input type="email" value="'.$data->email.'" name="user[email]"/></td></tr>';
-  echo '<tr><td><label for="homedir">Homedir</label></td><td><input type="text" value="'.$data->homedir.'" name="user[homedir]"/></td></tr>';
-  echo '<tr><td><label for="uid">Uid</label></td><td><input type="number" value="'.$data->uid.'" name="user[uid]"/></td></tr>';
-  echo '<tr><td><label for="gid">Gid</label></td><td><input type="number" value="'.$data->gid.'" name="user[gid]"/></td></tr>';
-  echo '<tr><td><label for="shell">Shell</label></td><td><input type="text" value="'.$data->shell.'" name="user[shell]"/></td></tr>';
-  echo '<tr><td><label for="status">Status</label></td><td><input type="checkbox" '.(((int) $data->status != 0)?'checked value="1"':'value="0"').' name="user[status]" /></td></tr>';
-  echo '<tr><td><label for="password">Password(Please input password if you want to change it.)</label></td><td><input type="password" value="" name="user[password]"/></td></tr>';
-  echo '<tr><td><label for="repassword">Repite password</label></td><td><input type="password" value="" name="user[repassword]"/></td></tr>';
-  echo '<tr><td></td><td><button id="saveUser">Save</button></td></tr>';
-?>
+ <tr>
+  <td>
+   <label for="login">Login</label></td><td><input type="text" readonly="true" value="$data->login" name="user[login]"/>
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <label for="username">User Name</label></td><td><input type="text" value="$data->username" name="user[username]"/>
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <label for="email">Email</label></td><td><input type="email" value="$data->email" name="user[email]"/>
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <label for="homedir">Homedir</label></td><td><input type="text" value="$data->homedir" name="user[homedir]"/>
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <label for="uid">Uid</label></td><td><input type="number" value="$data->uid" name="user[uid]"/>
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <label for="gid">Gid</label></td><td><input type="number" value="$data->gid" name="user[gid]"/>
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <label for="shell">Shell</label></td><td><input type="text" value="$data->shell" name="user[shell]"/>
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <label for="status">Status</label></td><td><input type="checkbox" (((int) $data->status != 0) ? 'checked value="1"' : 'value="0"') name="user[status]" />
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <label for="password">Password(Please input password if you want to change it.)</label></td><td><input type="password" value="" name="user[password]"/>
+  </td>
+ </tr>
+ <tr>
+  <td>
+   <label for="repassword">Repite password</label></td><td><input type="password" value="" name="user[repassword]"/>
+  </td>
+ </tr>
+ <tr>
+  <td>
+   </td><td><button data-id="$data->userid" id="saveUser">Save</button>
+  </td>
+ </tr>
  </tbody>
 </table>
 </p>
+EOT;
