087bb4714152a45f0de97058a0edd65244425d0a
zxzharmlesszxz <zxzharmlesszxz@gmail.com>
2016-12-10 20:39:00 +0200

    Update admins_view.php

--- application/views/admins_view.php
+++ application/views/admins_view.php
@@ -1,11 +1,12 @@
 <h1>Admins</h1>
-<p>
+<p class="hide">
 <input type="text" value="" placeholder="login" name="admin[login]"/>
 <input type="password" value="" placeholder="password" name="admin[password]"/>
 <input type="text" value="" placeholder="username" name="admin[username]"/>
 <input type="text" value="" placeholder="email" name="admin[email]"/>
  <button id="createAdmin">Create new Admin</button>
 </p>
+<p><button alt="Add new Admin" title="Add new Admin" id="show">Add new Admin</button></p>
 <p>
 <table id='table' class='display'>
 <thead>
