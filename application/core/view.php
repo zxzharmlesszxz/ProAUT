616945042e643995cdc517675e47877491173ba1
harmless <harmless@syneforge.com>
2017-01-13 11:30:43 +0200

    Add debug function to view class

--- application/core/view.php
+++ application/core/view.php
@@ -5,12 +5,16 @@
 **/
 
 class View {
-	public function generate($content_view, $template_view, $data = null) {
-		include 'application/views/'.$template_view;
-	}
+ public function generate($content_view, $template_view, $data = null) {
+  include 'application/views/'.$template_view;
+ }
 
- public function ajax($data) {
+ public function ajax($data = null) {
   header('Content-Type: application/json');
   echo json_encode($data);
+ }
+
+ public function debug($data = null) {
+  var_dump($data);
  }
 }
