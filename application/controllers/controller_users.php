896f3fe04754088083959074dc31fe124ac2a74e
harmless <harmless@syneforge.com>
2017-01-13 12:10:01 +0200

    Try to get user

--- application/controllers/controller_users.php
+++ application/controllers/controller_users.php
@@ -51,6 +51,7 @@ class Controller_Users extends Controller {
  public function action_show() {
   $login = $this->query;
   $users = $this->model->get_data();
+  $this->view->debug($this->query);
   foreach ($users as $user) {
    $this->view->debug($user);
    $data = ($user->login == $login) ? $user : null;
