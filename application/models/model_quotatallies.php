0cdbc51e327e84b462c471df9e7fba55bff1ebb2
harmless <harmless@syneforge.com>
2017-01-17 17:00:05 +0200

    Fix quotatally model(oh my mind)

--- application/models/model_quotatallies.php
+++ application/models/model_quotatallies.php
@@ -5,7 +5,7 @@ class Model_Quotatallies extends Model {
  public function get_data() {
   $quotatallys = new Collection;
   foreach (QuotaTally::find_all() as $quotatally) {
-   $quotatallys->addItem($quotatally, $quotatally->$quotatallyid);
+   $quotatallys->addItem($quotatally, $quotatally->quotatallyid);
   }
   return $quotatallys;
  }
