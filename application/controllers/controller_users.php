4df7338c82955e690c87d2edc6aff7d135924116
harmless <harmless@syneforge.com>
2017-01-17 15:17:10 +0200

    Fix

--- application/controllers/controller_users.php
+++ application/controllers/controller_users.php
@@ -49,8 +49,8 @@ class Controller_Users extends Controller {
    if ($users->getItem($userid)->login == $this->query['login']) {
     $data = $users->getItem($userid);
     if (config()->QUOTA) {
-     $quotalimit = QuotaLimit::find_by_scope(array('name' => $data->login));
-     $data->quotalimit = $quotalimit;//->bytes_in_avail;
+     $quotalimit = QuotaLimit::find_by_scope(array('name' => $data->login))[0];
+     $data->quotalimit = $quotalimit->bytes_in_avail;
     }
    }
   }
