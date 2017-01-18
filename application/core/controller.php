7c08e4fce8e6a2bab1610c42f98c6a353480e3a8
harmless <harmless@syneforge.com>
2017-01-18 14:42:55 +0200

    Test

--- application/core/controller.php
+++ application/core/controller.php
@@ -5,15 +5,19 @@
 **/
 
 abstract class Controller {
-	
-	public $model;
-	public $view;
-	
-	public function __construct() {
-		$this->view = new View();
-	}
+ 
+ public $model;
+ public $view;
+ 
+ public function __construct() {
+  $this->view = new View();
+ }
 
-	public function action_index() {
-		// todo	
-	}
+ public function action_index() {
+  // todo
+ }
+ 
+ public function action_error() {
+  $this->view->generate('404_view.php', 'template_view.php', "Method doesn't exists");
+ }
 }
--- application/core/route.php
+++ application/core/route.php
@@ -6,61 +6,54 @@
 
 class Route {
 
-	static public function start() {
-		$controller_name = 'Main';
-		$action_name = 'index';
-		
-		$routes = explode('/', $_SERVER['REQUEST_URI']);
+ static public function start() {
+  $controller_name = 'Main';
+  $action_name = 'index';
+  
+  $routes = explode('/', $_SERVER['REQUEST_URI']);
 
-		if (!empty($routes[1])) {	
-			$controller_name = $routes[1];
-		}
-		
-		if (!empty($routes[2])) {
-			$action_name = $routes[2];
-		}
+  if (!empty($routes[1])) { 
+   $controller_name = $routes[1];
+  }
+  
+  if (!empty($routes[2])) {
+   $action_name = $routes[2];
+  }
 
-		$model_name = 'Model_'.$controller_name;
-		$controller_name = 'Controller_'.$controller_name;
-		$action_name = 'action_'.$action_name;
+  $model_name = 'Model_'.$controller_name;
+  $controller_name = 'Controller_'.$controller_name;
+  $action_name = 'action_'.$action_name;
 
-		/*
-		echo "Model: $model_name <br>";
-		echo "Controller: $controller_name <br>";
-		echo "Action: $action_name <br>";
-		*/
+  /*
+  echo "Model: $model_name <br>";
+  echo "Controller: $controller_name <br>";
+  echo "Action: $action_name <br>";
+  */
 
-		$model_file = strtolower($model_name).'.php';
-		$model_path = "application/models/".$model_file;
+  $model_file = strtolower($model_name).'.php';
+  $model_path = "application/models/".$model_file;
 
-		if (file_exists($model_path)) {
-			include "application/models/".$model_file;
-		}
+  if (file_exists($model_path)) {
+   include "application/models/".$model_file;
+  }
 
-		$controller_file = strtolower($controller_name).'.php';
-		$controller_path = "application/controllers/".$controller_file;
+  $controller_file = strtolower($controller_name).'.php';
+  $controller_path = file_exists("application/controllers/".$controller_file) ? "application/controllers/".$controller_file : "application/controllers/controller_404.php";
 
-		if (file_exists($controller_path)) {
-			include "application/controllers/".$controller_file;
-		} else {
-			Route::ErrorPage404();
-		}
+  include $controller_path;
 
-		$controller = new $controller_name;
-		$action = $action_name;
-		
-		if (method_exists($controller, $action)) {
-		 $controller->$action();
-		} else {
-      Route::ErrorPage404();
-		}
-	}
+  $controller = class_exists($controller_name) ? new $controller_name : new Controller_404;
+
+  if (method_exists($controller, $action_name)) {
+   $controller->$action();
+  } else {
+   $controller->action_error();
+  }
+ }
 
  public function ErrorPage404() {
-  $host = '//'.$_SERVER['HTTP_HOST'].'/';
   header('HTTP/1.1 404 Not Found');
-  header("Status: 404 Not Found");
-  header('Location:'.$host.'404');
-  exit;
+  header('Status: 404 Not Found');
+  header('Location:/404');
  }
 }
