ed5fbbc857f958be8c6b35e996e244d6819ab02a
harmless <harmless@syneforge.com>
2017-01-24 10:49:56 +0200

    Commented namespaces

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -7,20 +7,20 @@ require_once 'core/controller.php';
 require_once 'core/route.php';
 
 $config = Config::getInstance();
-\Core\Registry::_set('config', $config);
+Registry::_set('config', $config);
 $database = new MySQL_Database;
-Core\Registry::_set('database', $database);
+Registry::_set('database', $database);
 
 function __autoload($class){
  @include_once __DIR__."/classes/${class}.class.php";
 }
 
 function config(){
- return \Core\Registry::_get('config');
+ return Registry::_get('config');
 }
 
 function db(){
- return \Core\Registry::_get('database');
+ return Registry::_get('database');
 }
 
-\Core\Route::start();
+Route::start();
